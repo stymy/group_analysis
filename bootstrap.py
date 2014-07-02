19340e7fc6be0ba9220aececd63335a68e394029
stymy <rschadmin@cmi-rsch-li002.childmind.org>
2014-07-02 13:42:44 -0400

    iterative bootstrapping

--- bootstrap.py
+++ bootstrap.py
@@ -1,6 +1,10 @@
+import pandas
 import os
 import nibabel as nb
 import numpy as np
+from group_spat_corr import do_it as correlate
+
+correlations = pandas.DataFrame()
 
 pipelines = ['cpac']
 strategies = ['filt_global','filt_noglobal']
@@ -13,13 +17,17 @@ pipeline = pipelines[0]
 pipeline2 = pipelines[0]
 derivative = derivatives[0]
 
+#outputs
+corrs = pandas.DataFrame()
+concs = pandas.DataFrame()
+spearmans = pandas.DataFrame()
+entropies = pandas.DataFrame()
+dices = pandas.DataFrame()
+
+
+#LOAD ORIGINAL FILES
 in_file = 'stats_%s_%s_%s/thresh_corrected_merged.nii.gz' %(pipeline, strategy, derivative)
 in_file_2 = 'stats_%s_%s_%s/thresh_corrected_merged.nii.gz' %(pipeline2, strategy2, derivative)
-
-output_dir = '/home/ubuntu/bootstraps_%s_%s_%s_v_%s_%s_%s' %(pipeline, strategy, derivative, pipeline2, strategy2, derivative)
-
-if not os.path.exists(output_dir):
-    os.mkdir(output_dir)
 
 X = nb.load(in_file).get_data().flatten()
 Y = nb.load(in_file_2).get_data().flatten()
@@ -27,7 +35,31 @@ Y = nb.load(in_file_2).get_data().flatten()
 if len(X) != len(Y):
     "WARNING arrays are different lengths, will use the length of %s to shuffle" %(in_file)
 
-scrambling_mat = np.random.permutation(len(X))
+#OUTPUT DIR
+output_dir = 'bootstraps_%s_%s_%s_v_%s_%s_%s' %(pipeline, strategy, derivative, pipeline2, strategy2, derivative)
 
-newX = X[scrambling_mat]
-newY = Y[scrambling_mat]
+if not os.path.exists(output_dir):
+    os.mkdir(output_dir)
+
+iterations = 10
+#MAKE PERMUTATIONS
+for i in xrange(iterations):
+    index = "iter"+str(i)
+
+    scrambling_mat = np.random.permutation(len(X))
+
+    newX = X[scrambling_mat]
+    newY = Y[scrambling_mat]
+
+    #CORRELATE
+    corr, conc, spear, dice, ecc =  correlate(newX,newY,do_entropy=False)
+    correlations.set_value(index,'pearson',corr)
+    correlations.set_value(index,'concordance',conc)
+    correlations.set_value(index,'spearman',spear)
+    correlations.set_value(index,'dice',dice[0])
+    correlations.set_value(index,'ecc',ecc)
+
+    #SAVE
+    np.save(os.path.join(output_dir,'strap_%s'%(index)), scrambling_mat)
+
+correlations.to_pickle(os.path.join(output_dir,'spat_correlations_%s.pd'%(iterations)))
--- group_spat_corr.py
+++ group_spat_corr.py
@@ -8,96 +8,107 @@ import pandas
 from scipy.stats import spearmanr
 import npeet.entropy_estimators as ee
 
-pipelines = ['cpac']
-strategies = ['filt_noglobal','filt_global', 'nofilt_global','nofilt_noglobal']
-derivatives = ['reho', 'alff','degree_weighted','degree_binarize','eigenvector_weighted','lfcd', 'falff']# 'eigenvector_binarize', vmhc
+def do_it(X,Y,do_entropy=True):
+    #pearsons
+    corr = np.corrcoef(X,Y)[0,1]
+    varx = np.var(X)
+    vary = np.var(Y)
+    cov = np.cov(X,Y)[0,1]
+    meanx = np.mean(X)
+    meany = np.mean(Y)
 
-derivs = pandas.Series()
-
-for derivative in derivatives:
-
-    corrs = pandas.DataFrame()
-    concs = pandas.DataFrame()
-    spearmans = pandas.DataFrame()
-    entropies = pandas.DataFrame()
-    dices = pandas.DataFrame()
-
-    for pipeline in pipelines:
-        for strategy in strategies:
-
-            #THIS MAKES SURE THAT DATA IS THERE, IF NOT, RUNS CORRESPONDING THE GROUP ANALYSIS SCRIPT
-            if not os.path.exists('stats_%s_%s_%s' %(pipeline, strategy, derivative)):
-                group_analysis.do_it(pipeline, strategy, derivative)
-
-            if not os.path.exists('stats_%s_%s_%s/thresh_corrected_merged.nii.gz' %(pipeline, strategy, derivative)):
-                output_image.do_it(pipeline, strategy, derivative)
-
-            #NOW compare between pipelines/strategies
-            in_file = 'stats_%s_%s_%s/thresh_corrected_merged.nii.gz' %(pipeline, strategy, derivative)
-
-            for pipeline2 in pipelines:
-                for strategy2 in strategies:
-                    in_file_2 = 'stats_%s_%s_%s/thresh_corrected_merged.nii.gz' %(pipeline2, strategy2, derivative)
-                    if not os.path.exists(in_file_2):
-                        group_analysis.do_it(pipeline2, strategy2, derivative)
-
-                    mask_file = 'stats_%s_%s_%s/mask.nii.gz' %(pipeline, strategy, derivative)
+    #concordance
+    conc = 2.*float(cov)/(float(varx)+float(vary)+np.square(float(meanx)-float(meany)))
+    
+    #spearman
+    spearman=spearmanr(X,Y)[0]
                     
-                    # make a spatial correlation matrix
-                    if not in_file == in_file_2:
-                        dot = '%s_%s_%s_v_%s_%s_%s' %(pipeline, strategy, derivative, pipeline2, strategy2, derivative)
-                        print dot
-                        d = subprocess.Popen(["3ddot", "-dosums", in_file, in_file_2], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                        stdout, stderr = d.communicate()
-                        # get values
-                        meanx, meany, varx, vary, cov, corr = stdout.strip().split()
+    #dice coefficient
+    U = np.union1d(X,Y)
+    dice = pandas.Series()
+    for percent in xrange(100):
+        threshold = np.percentile(abs(U),percent)
+        x = abs(X)>threshold
+        y = abs(Y)>threshold
+        s = x*y
+        xplusy = x.sum()+y.sum()
+        if  xplusy == 0:
+            die = -1
+        else:
+            die = 2.*s.sum()/xplusy
+            dice[str(percent)+'_'+str(threshold)] = die
 
-                        #pearson's correlation
-                        corrs.set_value(pipeline+'_'+strategy, pipeline2+'_'+strategy2, float(corr))
+    if do_entropy:
+        #entropy
+        x = ee.vectorize(X)
+        y = ee.vectorize(Y)
+        mi = ee.mi(x,y)
+        hx = -ee.entropy(x)
+        hy = -ee.entropy(y)
+        ecc= np.sqrt(mi/(0.5*(hx+hy)))
+    else:
+        ecc = np.nan
 
-                        #concordance
-                        conc = 2.*float(cov)/(float(varx)+float(vary)+np.square(float(meanx)-float(meany)))
-                        concs.set_value(pipeline+'_'+strategy, pipeline2+'_'+strategy2, conc)
+    return corr, conc, spearman, dice, ecc
 
-                        #spearman
-                        X = nb.load(in_file).get_data().flatten()
-                        Y = nb.load(in_file_2).get_data().flatten()
-                        spearman=spearmanr(X,Y)[0]
-                        spearmans.set_value(pipeline+'_'+strategy, pipeline2+'_'+strategy2, spearman)
-                        
-                        #dice coefficient
-                        U = np.union1d(X,Y)
-                        dice = pandas.Series()
-                        for percent in xrange(100):
-                            threshold = np.percentile(abs(U),percent)
-                            x = abs(X)>threshold
-                            y = abs(Y)>threshold
-                            s = x*y
-                            xplusy = x.sum()+y.sum()
-                            if  xplusy == 0:
-                                die = -1
-                            else:
-                                die = 2.*s.sum()/xplusy
-                            print die
-                            dice[str(percent)+'_'+str(threshold)] = die
-                        dices[pipeline+'_'+strategy+'_V_'+pipeline2+'_'+strategy2] = dice.values
+if __name__ == "__main__":
 
-                        #entropy
-                        x = ee.vectorize(X)
-                        y = ee.vectorize(Y)
-                        mi = ee.mi(x,y)
-                        hx = -ee.entropy(x)
-                        hy = -ee.entropy(y)
-                        ecc= np.sqrt(mi/(0.5*(hx+hy)))
-                        entropies.set_value(pipeline+'_'+strategy, pipeline2+'_'+strategy2, ecc)
+    pipelines = ['cpac']
+    strategies = ['filt_noglobal','filt_global', 'nofilt_global','nofilt_noglobal']
+    derivatives = ['reho', 'alff','degree_weighted','degree_binarize','eigenvector_weighted','lfcd', 'falff']# 'eigenvector_binarize', vmhc
 
-    #SAVE TO SERIES 
-    print derivative
-    print corrs
-    correlations = pandas.Series([corrs, concs, spearmans, entropies, dices], index=['pearson','concordance','spearmans','entropy','dice'],name=derivative)
-    derivs[derivative] = correlations
-    derivs.to_pickle('/home/ubuntu/spat_corr_dataframes')
+    derivs = pandas.Series()
 
+    for derivative in derivatives:
+
+        corrs = pandas.DataFrame()
+        concs = pandas.DataFrame()
+        spearmans = pandas.DataFrame()
+        entropies = pandas.DataFrame()
+        dices = pandas.DataFrame()
+
+        for pipeline in pipelines:
+            for strategy in strategies:
+
+                #THIS MAKES SURE THAT DATA IS THERE, \
+                #IF NOT, RUNS CORRESPONDING THE GROUP ANALYSIS SCRIPT
+                if not os.path.exists('stats_%s_%s_%s' %(pipeline, strategy, derivative)):
+                    group_analysis.do_it(pipeline, strategy, derivative)
+
+                if not os.path.exists('stats_%s_%s_%s/thresh_corrected_merged.nii.gz' %(pipeline, strategy, derivative)):
+                    output_image.do_it(pipeline, strategy, derivative)
+
+                #NOW compare between pipelines/strategies
+                in_file = 'stats_%s_%s_%s/thresh_corrected_merged.nii.gz' %(pipeline, strategy, derivative)
+
+                for pipeline2 in pipelines:
+                    for strategy2 in strategies:
+                        in_file_2 = 'stats_%s_%s_%s/thresh_corrected_merged.nii.gz' %(pipeline2, strategy2, derivative)
+                        if not os.path.exists(in_file_2):
+                            group_analysis.do_it(pipeline2, strategy2, derivative)
+
+                        mask_file = 'stats_%s_%s_%s/mask.nii.gz' %(pipeline, strategy, derivative)
+                        # make a spatial correlation matrix
+                        print '%s_%s_%s_v_%s_%s_%s' %(pipeline, strategy, derivative, pipeline2, strategy2, derivative)
+
+                        if not in_file == in_file_2:
+                            X = nb.load(in_file).get_data().flatten()
+                            Y = nb.load(in_file_2).get_data().flatten()
+                            corr, conc, spearman, dice, ecc =  do_it(X,Y)
+                            corrs.set_value(pipeline+'_'+strategy, pipeline2+'_'+strategy2, corr)
+                            concs.set_value(pipeline+'_'+strategy, pipeline2+'_'+strategy2, conc)
+                            spearmans.set_value(pipeline+'_'+strategy, pipeline2+'_'+strategy2, spearman)
+                            dices[pipeline+'_'+strategy+'_V_'+pipeline2+'_'+strategy2] = dice.values
+                            entropies.set_value(pipeline+'_'+strategy, pipeline2+'_'+strategy2, ecc)
+
+        #SAVE TO SERIES 
+        print derivative
+        correlations = pandas.Series([corrs, concs, spearmans, entropies, dices], \
+        index=['pearson','concordance','spearmans','entropy','dice'],name=derivative)
+        
+        derivs[derivative] = correlations
+        derivs.to_pickle('spat_corr_dataframes')
+    
     #TO ANALYZE, LOAD LIKE THIS:
     #a = pandas.read_pickle('/home/ubuntu/spat_corr_dataframes')
     #a.falff.pearson.cpac_filt_global.cpac_nofilt_global

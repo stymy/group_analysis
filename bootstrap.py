7163110d1980294b9f8fda950619c6b28fb0e344
stymy <rschadmin@cmi-rsch-li002.childmind.org>
2014-07-02 14:18:52 -0400

    improved loop and printing progress

--- bootstrap.py
+++ bootstrap.py
@@ -2,64 +2,58 @@ import pandas
 import os
 import nibabel as nb
 import numpy as np
+import itertools as it
 from group_spat_corr import do_it as correlate
 
 correlations = pandas.DataFrame()
 
 pipelines = ['cpac']
-strategies = ['filt_global','filt_noglobal']
-derivatives = ['reho']
+strategies = ['filt_global','filt_noglobal', 'nofilt_global', 'nofilt_noglobal']
+derivatives = ['reho','alff']
 
-#testing
-strategy = strategies[0]
-strategy2 = strategies[1]
-pipeline = pipelines[0]
-pipeline2 = pipelines[0]
-derivative = derivatives[0]
+for derivative in derivatives:
+    for pipeline, pipeline2 in it.combinations_with_replacement(pipelines,2):
+        for strategy, strategy2 in it.combinations(strategies,2):
+            #PRINT PROGRESS
+            print "%s: %s %s VERSUS %s %s" %(derivative, pipeline, strategy, pipeline2, strategy2)
 
-#outputs
-corrs = pandas.DataFrame()
-concs = pandas.DataFrame()
-spearmans = pandas.DataFrame()
-entropies = pandas.DataFrame()
-dices = pandas.DataFrame()
+            #LOAD ORIGINAL FILES
+            in_file = 'stats_%s_%s_%s/thresh_corrected_merged.nii.gz' %(pipeline, strategy, derivative)
+            in_file_2 = 'stats_%s_%s_%s/thresh_corrected_merged.nii.gz' %(pipeline2, strategy2, derivative)
 
+            X = nb.load(in_file).get_data().flatten()
+            Y = nb.load(in_file_2).get_data().flatten()
 
-#LOAD ORIGINAL FILES
-in_file = 'stats_%s_%s_%s/thresh_corrected_merged.nii.gz' %(pipeline, strategy, derivative)
-in_file_2 = 'stats_%s_%s_%s/thresh_corrected_merged.nii.gz' %(pipeline2, strategy2, derivative)
+            if len(X) != len(Y):
+                "WARNING arrays are different lengths, will use the length of %s to shuffle" %(in_file)
 
-X = nb.load(in_file).get_data().flatten()
-Y = nb.load(in_file_2).get_data().flatten()
+            #OUTPUT DIR
+            output_dir = 'bootstraps_%s_%s_v_%s_%s' %(pipeline, strategy, pipeline2, strategy2)
+            if not os.path.exists(output_dir):
+                os.mkdir(output_dir)
 
-if len(X) != len(Y):
-    "WARNING arrays are different lengths, will use the length of %s to shuffle" %(in_file)
+            iterations = 10
+            #MAKE PERMUTATIONS
+            for i in xrange(iterations):
+                index = "iter"+str(i)
 
-#OUTPUT DIR
-output_dir = 'bootstraps_%s_%s_%s_v_%s_%s_%s' %(pipeline, strategy, derivative, pipeline2, strategy2, derivative)
+                scrambling_mat = np.random.permutation(len(X))
 
-if not os.path.exists(output_dir):
-    os.mkdir(output_dir)
+                newX = X[scrambling_mat]
+                newY = Y[scrambling_mat]
 
-iterations = 10
-#MAKE PERMUTATIONS
-for i in xrange(iterations):
-    index = "iter"+str(i)
+                #CORRELATE
+                corr, conc, spear, dice, ecc =  correlate(newX,newY,do_entropy=False)
+                correlations.set_value(index,'pearson',corr)
+                correlations.set_value(index,'concordance',conc)
+                correlations.set_value(index,'spearman',spear)
+                correlations.set_value(index,'dice',dice[0])
+                correlations.set_value(index,'ecc',ecc)
 
-    scrambling_mat = np.random.permutation(len(X))
+                #SAVE
+                np.save(os.path.join(output_dir,'strap_%s'%(index)), scrambling_mat)
 
-    newX = X[scrambling_mat]
-    newY = Y[scrambling_mat]
+            #PRINT MEANS
+            print correlations.mean()
 
-    #CORRELATE
-    corr, conc, spear, dice, ecc =  correlate(newX,newY,do_entropy=False)
-    correlations.set_value(index,'pearson',corr)
-    correlations.set_value(index,'concordance',conc)
-    correlations.set_value(index,'spearman',spear)
-    correlations.set_value(index,'dice',dice[0])
-    correlations.set_value(index,'ecc',ecc)
-
-    #SAVE
-    np.save(os.path.join(output_dir,'strap_%s'%(index)), scrambling_mat)
-
-correlations.to_pickle(os.path.join(output_dir,'spat_correlations_%s.pd'%(iterations)))
+            correlations.to_pickle(os.path.join(output_dir,'boot_%s.pd'%(derivative)))

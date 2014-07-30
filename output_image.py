2f9e39688d39f60a5fcbdbf7c5a22ac0f4922b19
stymy <rschadmin@cmi-rsch-li002.childmind.org>
2014-07-30 12:36:35 -0400

    plotting

--- output_image.py
+++ output_image.py
@@ -54,8 +54,8 @@ def do_it(pipeline, strategy, derivative):
 if __name__ == "__main__":
 
     pipelines = ['cpac']
-    strategies = ['filt_noglobal']#,'filt_global','nofilt_global','nofilt_noglobal']
-    derivatives = ['reho']#, 'degree_binarize','falff', 'alff','degree_weighted','degree_binarize','eigenvector_binarize','eigenvector_weighted','lfcd'] #vmhc, dual_regression
+    strategies = ['filt_noglobal','filt_global','nofilt_global','nofilt_noglobal']
+    derivatives = ['reho', 'degree_binarize','falff', 'alff','degree_weighted','degree_binarize','eigenvector_binarize','eigenvector_weighted','lfcd', 'vmhc', 'dual_regression0','dual_regression1','dual_regression2','dual_regression3','dual_regression4','dual_regression5','dual_regression6','dual_regression7','dual_regression8','dual_regression9']
 
     for pipeline in pipelines:
         for strategy in strategies:
